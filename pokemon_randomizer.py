3e45620ce501a473a4a85b7f57b0e4bf67cbd00d
Jose Wilhelm <josel.wilhelms@gmail.com>
2022-11-05 21:18:31 -0400

    pokemon_randomizer user program interface

--- pokemon_randomizer.py
+++ pokemon_randomizer.py
@@ -1,23 +1,86 @@
-from pokemon_assigner_class import PokemonAssigner
+"""
+User interface with the Pokemon Randomizer program
+"""
 
-# Get the necessary parameters
-game = int(input("Select game (1 for crystal and 2 for emerald): "))
-hard_mode = input("Select difficulty ('hard'/'easy'/'full random'): ")
-num_pokemon = int(input("How many pokemon do you want in your team? Max 6: "))
+from pokemon_assigner_class import PokemonAssigner, Game
 
-# Create the instance of the challenge
-pok_rand = PokemonAssigner(game, hard_mode, num_pokemon)
 
-# Filter out the good pokemons of the challenge if hard mode was selected
-if pok_rand.difficulty == 'hard' or pok_rand.difficulty == 'easy':
-    pok_rand.excludePokemons()
+def main() -> None:
+    """Program that makes use of the pokemon_assigner class"""
 
-while True:
-    print("Showing randomized pokemons: ")
-    pok_rand.getRandomTeam()
-    
-    leave = input("Do you wish to finish? yes/no: ")
-    if leave == "yes":
-        break
-    else:
-        print("")
+    print("Welcome! Randomize your pokemon team here!")
+
+    # Get team size
+    while True:
+        team_size = int(input("Select your team size: "))
+        if PokemonAssigner(team_size):
+            break
+        print("Invalid team size! Try again.")
+
+    # Initialize class
+    team = PokemonAssigner(team_size)
+
+    # Get game
+    while True:
+        print(
+            "Select your game:\
+            \n(1) for Crystal\
+            \n(2) for RSE(Ruby/Sapphire/Emerald)\
+            \n(3) for Platinum"
+        )
+        game = int(input("Game: "))
+        if team.assign_game(game):
+            team.get_all_pokemons()
+            break
+
+    # Optional: Get game if generation is RSE
+    if team.game is Game.RSE:
+        while True:
+            print(
+                "Select your game:\
+                \n(1) for Ruby\
+                \n(2) for Sapphire\
+                \n(3) for Emerald"
+            )
+            gen_game = int(input("Game: "))
+            if team.assign_gen_3_game(gen_game):
+                break
+
+    # Get difficulty
+    while True:
+        print(
+            "Select game difficulty:\
+            \n(1) for Easy(Bad pokemons filtered out)\
+            \n(2) for Normal(No changes)\
+            \n(3) for Hard(Good pokemons filtered out)"
+        )
+        difficulty = int(input("Difficulty: "))
+        if team.assign_difficulty(difficulty):
+            team.get_all_pokemons()
+            break
+
+    # Get random team
+    while True:
+        team.get_team()
+        print(f"Your random team is: {team.assigned_team}")
+        print(
+            "What do you want to do now?\
+            \n(1) Get another random team\
+            \n(2) Start from the beginning\
+            \n(3) Exit"
+        )
+        option = int(input("Your option: "))
+
+        if option == 1:
+            continue
+        if option == 2:
+            main()
+        if option == 3:
+            break
+        else:
+            print("Invalid option, terminating program.")
+            break
+
+
+if __name__ == "__main__":
+    main()
